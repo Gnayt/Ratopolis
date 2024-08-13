a54fc135bd7c1b673fd3918a4e38ccee4421ea29
Gnayt <152925056+Gnayt@users.noreply.github.com>
2024-08-13 23:01:13 +0000

    update chat

--- aptos/frontend/components/Chat.tsx
+++ aptos/frontend/components/Chat.tsx
@@ -26,7 +26,7 @@ const abi = { "address": "0x8c9e3104a866a70ebee3d3aab02ff66d5d7a70993f0f04899f54
         "is_view": false,
         "generic_type_params": [],
         "params": ["&signer","address","string"],
-        "return": []
+        "return": ["string"]
     },
     {
         "name": "add_component",
@@ -82,8 +82,6 @@ const abi = { "address": "0x8c9e3104a866a70ebee3d3aab02ff66d5d7a70993f0f04899f54
           }
     ] } as const;
 export default function Chat() {
-    const [history, setHistory] = useState([] as Message[]);
-    const [message, setMessage] = useState("hello");
     const { account } = useWallet();
 
     const {
@@ -96,11 +94,18 @@ export default function Chat() {
     } = useSubmitTransaction();
 
     const postMessage = async () => {
+        const recipientAddress = account?.address;
+
+        // Check if the recipient address is defined
+        if (!recipientAddress) {
+            throw new Error('Wallet not connected');
+        }
         try {
+            let test = 'test'
             const payload = createEntryPayload(abi, {
                 function: 'mint',
                 typeArguments: [],
-                functionArguments: [[], abi.address,''],
+                functionArguments: [recipientAddress,2,test],
 
             });
             const tx = await submitTransaction(payload);
@@ -113,24 +118,6 @@ export default function Chat() {
 
     return (
         <div className="grid grid-flow-col-1 group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 dark:border-neutral-700 dark:bg-neutral-800/30">
-            <h1 className="text-lg">History</h1>
-            <div id="scrollableContainer" className="overflow-y-scroll h-72">
-
-
-                {history.map((message, index) => (
-
-                    <div key={index} className={message.sender == account?.address ? "text-right border-b-[1px] border-stone-700 p-2" : "border-b-[1px] border-stone-700 p-2"}>
-                        <p className={message.sender == account?.address ? "text-stone-400" : ""}>{message.sender == account?.address ? "You: " : formatAddress(message.sender as string) + ": "}{hex2a(message.text.slice(2))}</p>
-
-                        <p className="text-xs">{formatDate(new Date(message.timestamp * 1000))}</p>
-                    </div>
-                ))}
-            </div>
-            <form className="" onSubmit={(e) => { e.preventDefault(); postMessage(); }}>
-                <label className="p-2">Message
-                    <input type="text" className="z-2000 text-white bg-black p-2 m-2 border-[1px]ot " defaultValue={"hello"} onChange={updateMessage} />
-                </label>
-            </form>
             <button className="bg-black border-gray-300 rounded-lg border-[1px] text-white" onClick={postMessage}>Send</button>
         </div>
     )
